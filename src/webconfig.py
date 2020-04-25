ed9ad16149767f5d16ed9634b9bc412b1f6ba9b6
ckl67 <christian.klugesherz@gmail.com>
2020-04-26 00:48:39 +0200

    Fix Installation error #2

--- src/webconfig.py
+++ src/webconfig.py
@@ -2,8 +2,8 @@ from ctypes import *
 
 class Config(object):
     WEBAPP_DEBUG_LEVEL = 0  # 0 to 5 
-    WEBAPP_VERSION = "1.00"
-    TAP2WAV_VERSION = "1.12"
+    WEBAPP_VERSION = "1.0.2"
+    TAP2WAV_VERSION = "1.1.2"
     SECRET_KEY = 'Mon Secret se cache derrière'
     TAP2WAV_FORM_FREQUENCE_ID = 3
     TAP2WAV_FORM_SPLIT_ID = 1
